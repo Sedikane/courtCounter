684f3e7ab59503b0942f63816052aa64f13fa398
Sedikane <mogorueve@gmail.com>
2018-06-27 09:24:28 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+teamA: number;
+teamB: number;
   constructor(public navCtrl: NavController) {
-
+    this.teamA = 0;
+    this.teamB = 0;
   }
+Click(){
+ this.teamA +=1;
+}
+ClickTwo(){
+  this.teamA +=2;
 
+}
+ClickThree(){
+  this.teamA +=3;
+}
+Click1(){
+  this.teamB +=2;
+}
+Click2(){
+  this.teamB +=2;
+}
+Click3(){
+  this.teamB +=3;
+}
+myClick() {
+  this.teamA = 0;
+  this.teamB = 0;
+}
 }
